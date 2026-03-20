aea12840fe58901b731934bdf3ad5e20de7f387a
박명환 <jackpark5872@yonsei.ac.kr>
2026-03-20 14:19:52 +0900

    style: 코드 들여쓰기 변경

--- analytics/src/main/java/com/aitale/analytics/analytics/dto/request/StudyResultCreateRequest.java
+++ analytics/src/main/java/com/aitale/analytics/analytics/dto/request/StudyResultCreateRequest.java
@@ -3,15 +3,14 @@ package com.aitale.analytics.analytics.dto.request;
 import java.time.LocalDateTime;
 
 public record StudyResultCreateRequest(
-    Long userId,
-    Long storyId,
-    String genre,
-    LocalDateTime solvedAt,
-    Integer totalScore,
-    Integer quizCount,
-    Integer correctCount,
-    Integer wrongCount,
-    Integer currentLevel
-) {
+        Long userId,
+        Long storyId,
+        String genre,
+        LocalDateTime solvedAt,
+        Integer totalScore,
+        Integer quizCount,
+        Integer correctCount,
+        Integer wrongCount,
+        Integer currentLevel) {
 
 }
