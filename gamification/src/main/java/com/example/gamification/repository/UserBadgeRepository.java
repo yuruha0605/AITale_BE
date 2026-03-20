66d7f83f694213516da3663aac88c06533b92989
alsrbs915-jpg <alsrbs915@gmail.com>
2026-03-20 11:33:45 +0900

    Feat: 사용자 경험치 및 배지 조회 API 로직 구현 완료

--- gamification/src/main/java/com/example/gamification/repository/UserBadgeRepository.java
+++ gamification/src/main/java/com/example/gamification/repository/UserBadgeRepository.java
@@ -5,14 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
-    
+
     // 배지 중복 체크용
     boolean existsByUserIdAndBadgeId(Long userId, Long badgeId);
-<<<<<<< HEAD
 
     // 사용자별 전체 배지 조회용
     List<UserBadge> findAllByUserId(Long userId);
 }
-=======
-}
->>>>>>> 79eecd29d67268b1cfb7d8ae681127c961eaf057
