79eecd29d67268b1cfb7d8ae681127c961eaf057
박명환 <jackpark5872@yonsei.ac.kr>
2026-03-20 11:07:28 +0900

    문서 수정

--- learning/src/main/java/com/example/learning/service/DifficultyTestService.java
+++ learning/src/main/java/com/example/learning/service/DifficultyTestService.java
@@ -60,11 +60,11 @@ public class DifficultyTestService {
 
         // 난이도 판정
         Difficulty assignedDifficulty;
-        if (correctCount <= 3) {
+        if (correctCount <= 3)
             assignedDifficulty = Difficulty.LOW;
-        } else if (correctCount <= 7) {
+        else if (correctCount <= 7)
             assignedDifficulty = Difficulty.MEDIUM;
-        } else
+        else
             assignedDifficulty = Difficulty.HIGH;
 
         // // FeignClient로 user-service 호출
