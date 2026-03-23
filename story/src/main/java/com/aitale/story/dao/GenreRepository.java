48f870ad3417a335653d0fbf7447445d2200cb34
gyuoo <yeomky98@naver.com>
2026-03-23 23:14:52 +0900

    feat: 장르명을 조회하는 repository 메서드 추가

--- story/src/main/java/com/aitale/story/dao/GenreRepository.java
+++ story/src/main/java/com/aitale/story/dao/GenreRepository.java
@@ -1,8 +1,10 @@
 package com.aitale.story.dao;
 
+import com.aitale.story.domain.entity.GenreEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.aitale.story.domain.entity.GenreEntity;
-
 public interface GenreRepository extends JpaRepository<GenreEntity, Long> {
+
+    Optional<GenreEntity> findByGenreName(String genreName);
 }
