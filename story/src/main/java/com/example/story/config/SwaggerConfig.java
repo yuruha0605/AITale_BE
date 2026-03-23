b5c959671bebf500e6d5199df9d578c7a0d75961
박명환 <jackpark5872@yonsei.ac.kr>
2026-03-23 21:38:25 +0900

    docs: 스웨거가 /story-service를 볼 수 있도록 설정

--- story/src/main/java/com/example/story/config/SwaggerConfig.java
+++ story/src/main/java/com/example/story/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,6 +13,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("/story-service").description("Gateway route base path"))
                 .info(new Info()
                         .title("Story Service API 명세서")
                         .description("MSA 구조에서 스토리 도메인을 담당하는 API")
