af7cece4e4c39a606c5c934fbfa4f5fe587ea309
gyuoo <yeomky98@naver.com>
2026-03-24 13:22:53 +0900

    refactor: 주석 제거 및 Mock 데이터 제거

--- recommendation/src/main/java/com/aitale/recommendation/application/RecommendationCacheService.java
+++ recommendation/src/main/java/com/aitale/recommendation/application/RecommendationCacheService.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -20,52 +19,25 @@ public class RecommendationCacheService {
     private final StringRedisTemplate stringRedisTemplate;
     private final ObjectMapper objectMapper;
 
-//    public List<Recommendation> get(Long userId) {
-//        String value = stringRedisTemplate.opsForValue().get(generateKey(userId));
-//        if (value == null) {
-//            return List.of();
-//        }
-//
-//        try {
-//            return objectMapper.readValue(value, new TypeReference<>() {
-//            });
-//        } catch (JsonProcessingException exception) {
-//            return List.of();
-//        }
-//    }
+    public List<Recommendation> get(Long userId) {
+        String value = stringRedisTemplate.opsForValue().get(generateKey(userId));
+        if (value == null) {
+            return List.of();
+        }
 
-    // TODO: 테스트 성공 시 삭제
-    public Optional<List<Recommendation>> get(Long userId) {
         try {
-            String key = "recommendation:user:" + userId;
-            String value = stringRedisTemplate.opsForValue().get(key);
-
-            if (value == null) {
-                return Optional.empty();
-            }
-
-            List<Recommendation> recommendations =
-                objectMapper.readValue(value, new TypeReference<>() {
-                });
-            return Optional.of(recommendations);
-        } catch (Exception e) {
-            return Optional.empty();
+            return objectMapper.readValue(value, new TypeReference<>() {
+            });
+        } catch (JsonProcessingException exception) {
+            return List.of();
         }
     }
 
     public void put(Long userId, List<Recommendation> recommendations) {
-//        try {
-//            String value = objectMapper.writeValueAsString(recommendations);
-//            stringRedisTemplate.opsForValue().set(generateKey(userId), value, TTL);
-//        } catch (JsonProcessingException ignored) {
-//        }
-        // TODO: 테스트 성공 시 삭제
         try {
-            String key = "recommendation:user:" + userId;
             String value = objectMapper.writeValueAsString(recommendations);
-
-            stringRedisTemplate.opsForValue().set(key, value);
-        } catch (Exception e) {
+            stringRedisTemplate.opsForValue().set(generateKey(userId), value, TTL);
+        } catch (JsonProcessingException ignored) {
         }
     }
 
